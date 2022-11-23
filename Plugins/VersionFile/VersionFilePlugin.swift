fe8c761ab4e1f75e8309618ced29263fee187b0f
Matt Gacy <matt@mgacy.com>
2022-11-23 08:25:12 -0700

    Define platforms, plugin

---
+++ Plugins/VersionFile/VersionFilePlugin.swift
@@ -0,0 +1,20 @@
+//
+//  VersionFilePlugin.swift
+//  
+//
+//  Created by Mathew Gacy on 11/23/22.
+//
+
+import Foundation
+import PackagePlugin
+
+@main
+struct VersionFilePlugin: CommandPlugin {
+    /// This entry point is called when operating on a Swift package.
+    func performCommand(
+        context: PluginContext,
+        arguments: [String]
+    ) async throws {
+        // ...
+    }
+}
